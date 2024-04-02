c83aa84d826a7a43bc715e394ee2d4caabe35771
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-04-02 23:55:32 +0530

    Home Layout
    Basic Layout was created.

---
+++ Cart.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/icon" href="bi bi-telephone" class="rounded-circle">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <div class="container-fluid"></div>
+    
+</body>
+</html>
